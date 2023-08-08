62245274bfe59a3965f39b12781b3c39c2cd4d59
Azrubael <i0638464000@gmail.com>
2023-08-08 18:55:25 +0300

    Step #01. Initial commit. A frontend application in a folder `store-app`, bootstrapped with `create-next-app` and a prototype 230727-mern-store (see the previous project).

---
+++ README.md
@@ -0,0 +1,7 @@
+Educational project of the online store:
+- frontend - React;
+- backend - Node.js;
+- MongoDB;
+- rabbitmw;
+- notificztions - Node.js.
+
---
+++ store-app/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import App from './App'
+
+
+export default function Home() {
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <App />
+      </div>
+    </main>
+  )
+}
