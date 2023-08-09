0e4ceb1c25a1b2cffd99bb6da0dca562efa73e31
Azrubael <i0638464000@gmail.com>
2023-08-09 13:22:36 +0300

    Step #05. Testing the backens app `products-api` with a local network: $ cd products-api $ docker build -t azrubael/mern5shop:nodedb2 . $ docker network create e5shop $ docker run --name mongodb --network e5shop -p 27017:27017 -d -v data:/data/db mongo $ docker run --rm --name 5shop-nodedb1 --network e5shop azrubael/mern5shop:nodedb2

--- products-api/index.js
+++ products-api/index.js
@@ -87,7 +87,16 @@ app.get("/orders", async (req, res) => {
 mongoose.set('strictQuery', false)
 
 // const mongo_URI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGODB_HOST}/products` 
+
+// [3] - a local network approach (with a container name)
+const mongo_URI = 'mongodb://mongodb:27017/products'
+
+/*
+[2] - tested link, a little better approach
+const mongo_URI = 'mongodb://172.17.0.2:27017/products'
+[1] - tested link
 const mongo_URI = 'mongodb://host.docker.internal:27017/products'
+*/
 
 const options = {
     useNewUrlParser: true,
