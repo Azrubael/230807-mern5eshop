d463d2efd4cac979fe5b0a9e2685c1ab17dec808
Azrubael <i0638464000@gmail.com>
2023-08-15 11:29:30 +0300

    Step #21. A new `store-app-react` with updated dependensies through package.json and a new `createRoot` hook.

--- store-app-react/src/index.js
+++ store-app-react/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ChakraProvider>
 );
 
-reportWebVitals(sendToVercelAnalytics);
+reportWebVitals();
